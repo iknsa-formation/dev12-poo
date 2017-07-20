42cb1ce95e7573ee4239fc957997c694c5b7e2bc
Moustakime KIFIA <mkifia@icloud.com>
2017-07-20 12:07:28 +0200

    Auteur Model

--- src/Aston/BlogBundle/Entity/Auteur.php
+++ src/Aston/BlogBundle/Entity/Auteur.php
@@ -11,5 +11,68 @@ namespace Aston\BlogBundle\Entity;
 
 class Auteur
 {
+    private $nom;
+    private $prenom;
+    private $fonction;
 
+    public function __construct($data) {
+        $this->hydrate($data);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param mixed $prenom
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFonction()
+    {
+        return $this->fonction;
+    }
+
+    /**
+     * @param mixed $fonction
+     */
+    public function setFonction($fonction)
+    {
+        $this->fonction = $fonction;
+    }
+
+    public function hydrate (array $data) {
+        foreach($data as $key => $value) {
+            $method = 'set'.ucfirst($key);
+            if(method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
 }
